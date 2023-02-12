87a6756f62585d8789fdbb14a7c93d98f7e12d28
木亘 <8119755+wooden-joint@user.noreply.gitee.com>
2023-02-12 10:20:08 +0800

    fix-test-github-gitee

---
+++ src/components/scrollCircle.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+import { onMounted, reactive } from 'vue';
+
+</script>
+
+<template>
+  <div class='com-scroll-circle'></div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
